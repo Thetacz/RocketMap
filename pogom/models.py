7e508fa6d9e8fe62adad9184651f84c90eed70c7
Unknown <jakub.krysl@gmail.com>
2017-05-09 10:55:39 +0200

    replace default CharField() with Utf8mb4CharField()
    limits max_length to 191 chars (764 bytes)

--- pogom/models.py
+++ pogom/models.py
@@ -49,6 +49,13 @@ class MyRetryDB(RetryOperationalError, PooledMySQLDatabase):
     pass
 
 
+# Reduction of CharField to fit max length inside 767 bytes for utf8mb4 charset
+class Utf8mb4CharField(CharField):
+    def __init__(self, max_length=191, *args, **kwargs):
+        self.max_length = max_length
+        super(CharField, self).__init__(*args, **kwargs)
+
+
 def init_database(app):
     if args.db_type == 'mysql':
         log.info('Connecting to MySQL database on %s:%i...',
@@ -96,8 +103,8 @@ class BaseModel(flaskDb.Model):
 class Pokemon(BaseModel):
     # We are base64 encoding the ids delivered by the api
     # because they are too big for sqlite to handle.
-    encounter_id = CharField(primary_key=True, max_length=50)
-    spawnpoint_id = CharField(index=True, max_length=191)
+    encounter_id = Utf8mb4CharField(primary_key=True, max_length=50)
+    spawnpoint_id = Utf8mb4CharField(index=True)
     pokemon_id = SmallIntegerField(index=True)
     latitude = DoubleField()
     longitude = DoubleField()
@@ -436,13 +443,14 @@ class Pokemon(BaseModel):
 
 
 class Pokestop(BaseModel):
-    pokestop_id = CharField(primary_key=True, max_length=50)
+    pokestop_id = Utf8mb4CharField(primary_key=True, max_length=50)
     enabled = BooleanField()
     latitude = DoubleField()
     longitude = DoubleField()
     last_modified = DateTimeField(index=True)
     lure_expiration = DateTimeField(null=True, index=True)
-    active_fort_modifier = CharField(max_length=50, null=True, index=True)
+    active_fort_modifier = Utf8mb4CharField(max_length=50,
+                                            null=True, index=True)
     last_updated = DateTimeField(
         null=True, index=True, default=datetime.utcnow)
 
@@ -534,7 +542,7 @@ class Pokestop(BaseModel):
 
 class Gym(BaseModel):
 
-    gym_id = CharField(primary_key=True, max_length=50)
+    gym_id = Utf8mb4CharField(primary_key=True, max_length=50)
     team_id = SmallIntegerField()
     guard_pokemon_id = SmallIntegerField()
     gym_points = IntegerField()
@@ -702,7 +710,7 @@ class Gym(BaseModel):
 
 
 class LocationAltitude(BaseModel):
-    cellid = CharField(primary_key=True, max_length=50)
+    cellid = Utf8mb4CharField(primary_key=True, max_length=50)
     latitude = DoubleField()
     longitude = DoubleField()
     last_modified = DateTimeField(index=True, default=datetime.utcnow,
@@ -747,7 +755,7 @@ class LocationAltitude(BaseModel):
 
 
 class ScannedLocation(BaseModel):
-    cellid = CharField(primary_key=True, max_length=50)
+    cellid = Utf8mb4CharField(primary_key=True, max_length=50)
     latitude = DoubleField()
     longitude = DoubleField()
     last_modified = DateTimeField(
@@ -1084,9 +1092,9 @@ class ScannedLocation(BaseModel):
 
 
 class MainWorker(BaseModel):
-    worker_name = CharField(primary_key=True, max_length=50)
+    worker_name = Utf8mb4CharField(primary_key=True, max_length=50)
     message = TextField(null=True, default="")
-    method = CharField(max_length=50)
+    method = Utf8mb4CharField(max_length=50)
     last_modified = DateTimeField(index=True)
     accounts_working = IntegerField()
     accounts_captcha = IntegerField()
@@ -1109,15 +1117,15 @@ class MainWorker(BaseModel):
 
 
 class WorkerStatus(BaseModel):
-    username = CharField(primary_key=True, max_length=50)
-    worker_name = CharField(index=True, max_length=50)
+    username = Utf8mb4CharField(primary_key=True, max_length=50)
+    worker_name = Utf8mb4CharField(index=True, max_length=50)
     success = IntegerField()
     fail = IntegerField()
     no_items = IntegerField()
     skip = IntegerField()
     captcha = IntegerField()
     last_modified = DateTimeField(index=True)
-    message = CharField(max_length=255)
+    message = Utf8mb4CharField(max_length=191)
     last_scan_date = DateTimeField(index=True)
     latitude = DoubleField(null=True)
     longitude = DoubleField(null=True)
@@ -1188,13 +1196,13 @@ class WorkerStatus(BaseModel):
 
 
 class SpawnPoint(BaseModel):
-    id = CharField(primary_key=True, max_length=50)
+    id = Utf8mb4CharField(primary_key=True, max_length=50)
     latitude = DoubleField()
     longitude = DoubleField()
     last_scanned = DateTimeField(index=True)
     # kind gives the four quartiles of the spawn, as 's' for seen
     # or 'h' for hidden.  For example, a 30 minute spawn is 'hhss'.
-    kind = CharField(max_length=4, default='hhhs')
+    kind = Utf8mb4CharField(max_length=4, default='hhhs')
 
     # links shows whether a Pokemon encounter id changes between quartiles or
     # stays the same.  Both 1x45 and 1x60h3 have the kind of 'sssh', but the
@@ -1206,7 +1214,7 @@ class SpawnPoint(BaseModel):
     # Note index is shifted by a half. links[0] is the link between
     # kind[0] and kind[1] and so on. links[3] is the link between
     # kind[3] and kind[0]
-    links = CharField(max_length=4, default='????')
+    links = Utf8mb4CharField(max_length=4, default='????')
 
     # Count consecutive times spawn should have been seen, but wasn't.
     # If too high, will not be scheduled for review, and treated as inactive.
@@ -1422,11 +1430,11 @@ class ScanSpawnPoint(BaseModel):
 
 
 class SpawnpointDetectionData(BaseModel):
-    id = CharField(primary_key=True, max_length=54)
+    id = Utf8mb4CharField(primary_key=True, max_length=54)
     # Removed ForeignKeyField since it caused MySQL issues.
-    encounter_id = CharField(max_length=54)
+    encounter_id = Utf8mb4CharField(max_length=54)
     # Removed ForeignKeyField since it caused MySQL issues.
-    spawnpoint_id = CharField(max_length=54, index=True)
+    spawnpoint_id = Utf8mb4CharField(max_length=54, index=True)
     scan_time = DateTimeField()
     tth_secs = SmallIntegerField(null=True)
 
@@ -1621,7 +1629,7 @@ class SpawnpointDetectionData(BaseModel):
 
 
 class Versions(flaskDb.Model):
-    key = CharField()
+    key = Utf8mb4CharField()
     val = SmallIntegerField()
 
     class Meta:
@@ -1629,8 +1637,8 @@ class Versions(flaskDb.Model):
 
 
 class GymMember(BaseModel):
-    gym_id = CharField(index=True, max_length=191)
-    pokemon_uid = CharField(index=True, max_length=191)
+    gym_id = Utf8mb4CharField(index=True)
+    pokemon_uid = Utf8mb4CharField(index=True)
     last_scanned = DateTimeField(default=datetime.utcnow, index=True)
 
     class Meta:
@@ -1638,10 +1646,10 @@ class GymMember(BaseModel):
 
 
 class GymPokemon(BaseModel):
-    pokemon_uid = CharField(primary_key=True, max_length=50)
+    pokemon_uid = Utf8mb4CharField(primary_key=True, max_length=50)
     pokemon_id = SmallIntegerField()
     cp = SmallIntegerField()
-    trainer_name = CharField(index=True, max_length=191)
+    trainer_name = Utf8mb4CharField(index=True)
     num_upgrades = SmallIntegerField(null=True)
     move_1 = SmallIntegerField(null=True)
     move_2 = SmallIntegerField(null=True)
@@ -1658,17 +1666,17 @@ class GymPokemon(BaseModel):
 
 
 class Trainer(BaseModel):
-    name = CharField(primary_key=True, max_length=50)
+    name = Utf8mb4CharField(primary_key=True, max_length=50)
     team = SmallIntegerField()
     level = SmallIntegerField()
     last_seen = DateTimeField(default=datetime.utcnow)
 
 
 class GymDetails(BaseModel):
-    gym_id = CharField(primary_key=True, max_length=50)
-    name = CharField()
+    gym_id = Utf8mb4CharField(primary_key=True, max_length=50)
+    name = Utf8mb4CharField()
     description = TextField(null=True, default="")
-    url = CharField()
+    url = Utf8mb4CharField()
     last_scanned = DateTimeField(default=datetime.utcnow)
 
 
@@ -1706,7 +1714,7 @@ class Token(flaskDb.Model):
 
 
 class HashKeys(BaseModel):
-    key = CharField(primary_key=True, max_length=20)
+    key = Utf8mb4CharField(primary_key=True, max_length=20)
     maximum = SmallIntegerField(default=0)
     remaining = SmallIntegerField(default=0)
     peak = SmallIntegerField(default=0)
@@ -2712,12 +2720,13 @@ def database_migrate(db, old_ver):
 
     if old_ver < 2:
         migrate(migrator.add_column('pokestop', 'encounter_id',
-                                    CharField(max_length=50, null=True)))
+                                    Utf8mb4CharField(max_length=50,
+                                                     null=True)))
 
     if old_ver < 3:
         migrate(
             migrator.add_column('pokestop', 'active_fort_modifier',
-                                CharField(max_length=50, null=True)),
+                                Utf8mb4CharField(max_length=50, null=True)),
             migrator.drop_column('pokestop', 'encounter_id'),
             migrator.drop_column('pokestop', 'active_pokemon_id')
         )
